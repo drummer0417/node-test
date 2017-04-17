1942f713181fb2d7f70f7cd6d2fe6f99d56e4a5e
drummer0417 <hans@androidappfactory.nl>
2017-04-17 22:58:32 +0200

    Initial version

---
+++ utils/utils.js
@@ -0,0 +1,5 @@
+module.exports.add = (a, b) => a + b;
+
+module.exports.square = (x) => x * x;
+
+module.exports.devide = (a, b) => a / b;
---
+++ utils/utils.test.js
@@ -0,0 +1,35 @@
+//
+//
+// for docu about expect library see: https://github.com/mjackson/expect
+//
+//
+const expect = require('expect');
+
+const utils = require('./utils');
+
+it('should add 2 numbers', () => {
+    var testResult = utils.add(297, 120);
+    expect(testResult).toBe(417);
+});
+
+it('should square a number', () => {
+    var testValue = 9;
+    var expected = 81;
+    var result = utils.square(testValue);
+    expect(result).toBeA('number').toBe(81);
+})
+
+it('should devide two numbers', () => {
+    var x = 54;
+    var y = 4;
+    var expected = 13.5;
+    var result = utils.devide(x, y);
+    expect(result).toBeA('number').toBe(expected);
+});
+
+it('should expect some values', () => {
+    expect(12).toEqual(12);
+    expect([3, 4, 5]).toInclude(5);
+    expect([3, 4, 5]).toExclude(6);
+
+});
