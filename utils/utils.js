9f7429a6b29d0fe06b8b479dec07054cfa1ee963
drummer0417 <hans@androidappfactory.nl>
2017-04-18 23:13:12 +0200

    Added more test cases

--- utils/utils.js
+++ utils/utils.js
@@ -5,8 +5,11 @@ module.exports.square = (x) => x * x;
 module.exports.devide = (a, b) => a / b;
 
 module.exports.setName = (user, fullName) => {
-  var names = fullName.split(' ');
-  user.firstName = names[0];
-  user.lastName = names[1];
-  return user;
+    var names = fullName.split(' ');
+    // console.log(names);
+    user.firstName = names[0];
+    user.lastName = names[1];
+
+    // console.log(`User: ${JSON.stringify(user)}`);
+    return user;
 };
