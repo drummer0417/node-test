821f6b236b5ba012b76e7cd500eaeb3e8d2d5c0e
drummer0417 <hans@androidappfactory.nl>
2017-04-27 11:53:51 +0200

    Test SourceTree

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 module.exports.app = app.get('/', (req, res) => {
-    res.status(401).send({
-        bla: "ssdff",
-        error: "Page not found.",
-        bloe: "sdf"
-    });
+  res.status(401).send({
+    bla: "ssdff",
+    error: "Page not found.",
+    bloe: "Testing One, 2"
+  });
 });
 
 module.exports.app = app.get('/user', (req, res) => {
-    res.status(200).send({
-        "users": [{
-            "name": "Jacky",
-            "age": "50"
-        }, {
-            "name": "Hans",
-            "age": "55"
-        }, {
-            "name": "Cas",
-            "age": "14"
-        }, {
-            "name": "Anouk",
-            "age": "12"
-        }]
-    })
+  res.status(200).send({
+    "users": [{
+      "name": "Jacky",
+      "age": "50"
+    }, {
+      "name": "Hans",
+      "age": "55"
+    }, {
+      "name": "Cas",
+      "age": "14"
+    }, {
+      "name": "Anouk",
+      "age": "12"
+    }]
+  })
 });
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
+  console.log(`server started on port ${port}`);
 });
